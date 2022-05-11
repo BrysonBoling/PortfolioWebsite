4c83ae49ef6dace09d0dc392e58b43384d14fa54
Bryson Boling <brysonboling13@gmail.com>
2022-05-11 10:07:58 -0500

    Cleaned up some code and made few minor text edits

--- Portfolio/ClientApp/src/app/app.module.ts
+++ Portfolio/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ProjectsComponent } from './projects/projects.component';
 
@@ -17,8 +15,6 @@ import { ProjectsComponent } from './projects/projects.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     ResumeComponent,
     ProjectsComponent
   ],
@@ -28,8 +24,6 @@ import { ProjectsComponent } from './projects/projects.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'resume', component: ResumeComponent },
       { path: 'projects', component: ProjectsComponent },
     ])
